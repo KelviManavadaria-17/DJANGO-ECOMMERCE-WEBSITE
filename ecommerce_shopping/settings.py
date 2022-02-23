0bfaa1f1df81682a155faf172407b9ab4f33dc7c
unknown <kelvimanavadaria17012003@gmail.com>
2022-02-24 01:47:38 +0530

    HOSTED WITH HEROKU

--- ecommerce_shopping/settings.py
+++ ecommerce_shopping/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h$yt1#bth7)mhcvt8%%ypax30$j5fkp$s0zf(_h%etu#c^qo12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
